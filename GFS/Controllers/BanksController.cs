ac26b370aeea60fdce621df2f693ce5a09832697
Thokoza <thoh316@gmail.com>
2016-05-19 15:49:57 +0200

    Removal of unnecessary code

--- GFS/Controllers/BanksController.cs
+++ GFS/Controllers/BanksController.cs
@@ -19,10 +19,6 @@ namespace GFS.Controllers
         {
             return View(db.Banks.ToList());
         }
-        public string testThoks()
-        {
-            return null;
-        }
 
         // GET: Banks/Details/5
         public ActionResult Details(int? id)
